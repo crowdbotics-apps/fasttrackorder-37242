9f9e17abb5e4ed145eefbf03589378cb2cb5a448
s.moreshver@gmail.com <s.moreshver@gmail.com>
2022-12-10 08:05:54 -0500

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native";
-
 export const globalOptions = {
   name: "fasttrackorder_37242",
   url: "https://fasttrackorder_37242.botics.co",
   api: "https://fasttrackorder_37242.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     "copy": "Routes available!"
   }
-}
+};
